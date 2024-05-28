a1f9fa7aef92fc300d63306ca7f174a709bb4b2a
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-05-28 15:27:25 +0800

    Backend sign in, sign out, sign up

--- static/account.js
+++ static/account.js
@@ -13,3 +13,89 @@ async function fetch_user_account() {
 		show_front_page()
 	}
 }
+
+async function handle_sign_in(form) {
+	var fd = new FormData(form)
+
+	form["status"].value = "Please wait..."
+	form["status"].style.color = "inherit"
+
+	try {
+		var request = await fetch("/login", {
+			method: "POST",
+			body: fd
+		})
+	} catch (e) {
+		form["status"].value = "Network error"
+		form["status"].style.color = "red"
+		return
+	}
+
+	if (request.status == 200) {
+		form["status"].value = ""
+		fetch_user_account()
+		form.reset()
+	} else {
+		var response = await request.json()
+		form["status"].value = "Error: " + response.detail
+		form["status"].style.color = "red"
+	}
+}
+
+async function handle_sign_out(form) {
+	form["status"].value = "Please wait..."
+	form["status"].style.color = "inherit"
+
+	try {
+		var request = await fetch("/logout", {
+			method: "POST"
+		})
+	} catch (e) {
+		form["status"].value = "Network error"
+		form["status"].style.color = "red"
+		return
+	}
+
+	if (request.status == 200) {
+		form["status"].value = ""
+		fetch_user_account()
+	} else {
+		var response = await request.json()
+		form["status"].value = "Error: " + response.detail
+		form["status"].style.color = "red"
+	}
+}
+
+async function request_account_creation(form) {
+	var fd = new FormData(form)
+
+	var username = fd.get("username")
+	var password = fd.get("password")
+
+	form["status"].value = "Please wait..."
+	form["status"].style.color = "inherit"
+
+	try {
+		var request = await fetch("/register", {
+			method: "POST",
+			body: JSON.stringify({ username, password }),
+			headers: {
+				"Content-Type": "application/json"
+			},
+		})
+	} catch (e) {
+		form["status"].value = "Network error"
+		form["status"].style.color = "red"
+		return
+	}
+
+	if (request.status == 200) {
+		form["status"].value = ""
+		show_sign_up_success_page()
+		form.reset()
+	} else {
+		var response = await request.json()
+		form["status"].value = "Error: " + response.detail
+		form["status"].style.color = "red"
+	}
+}
--- static/app.js
+++ static/app.js
@@ -2,11 +2,19 @@
 
 var query = document.querySelector.bind(document)
 
-var app = {}
+var app = {
+	username: null,
+	account_page: HTMLElement.prototype, // Для подсказок
+	front_page: HTMLElement.prototype,
+	sign_up_page: HTMLElement.prototype,
+	sign_up_success_page: HTMLElement.prototype
+}
 
 function hide_all() {
 	app.front_page.classList.add("hide")
 	app.account_page.classList.add("hide")
+	app.sign_up_page.classList.add("hide")
+	app.sign_up_success_page.classList.add("hide")
 }
 
 function show_account_page() {
@@ -23,12 +31,26 @@ function show_front_page() {
 	app.front_page.classList.remove("hide")
 }
 
+function show_front_page() {
+	hide_all()
+	app.front_page.classList.remove("hide")
+}
+
+function show_sign_up_page() {
+	hide_all()
+	app.sign_up_page.classList.remove("hide")
+}
+
+function show_sign_up_success_page() {
+	hide_all()
+	app.sign_up_success_page.classList.remove("hide")
+}
+
 function app_init() {
-	app = {
-		username: null,
-		account_page: query(".account"),
-		front_page: query(".front_page")
-	}
+	app.front_page = query(".front_page")
+	app.account_page = query(".account")
+	app.sign_up_page = query(".sign_up")
+	app.sign_up_success_page = query(".sign_up_success")
 
 	fetch_user_account();
 }
